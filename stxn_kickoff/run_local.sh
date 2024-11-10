74a229317f722d3c506b83082f893612b01bbe5c
Evgeny Koshelev <eko2000@gmail.com>
2024-11-10 01:56:30 +0100

    Modify schedule param

--- stxn_kickoff/run_local.sh
+++ stxn_kickoff/run_local.sh
@@ -3,7 +3,7 @@ ws_chain_url=wss://service.lestnet.org:8888/
 laminator_address=0x36aB7A6ad656BC19Da2D5Af5b46f3cf3fc47274D
 call_breaker_address=0x23912387357621473Ff6514a2DC20Df14cd72E7f
 kitn_disbursement_scheduler_address=0x7E485Fd55CEdb1C303b2f91DFE7695e72A537399
-cron_schedule="0 * * * * *"
+cron_schedule="0 */3 * * * *"
 
 PROJECT_NAME="cleanup-mysql-v2"
 CURRENT_PROJECT=$(gcloud config get project)
