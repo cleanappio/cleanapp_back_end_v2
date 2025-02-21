d82eda956e5ad9a8d8b8b52a067f6a2be2832951
Evgeny Koshelev <eko2000@gmail.com>
2025-02-21 23:42:57 +0100

    Fix table creation in the db patch

--- db/patches/2.3.1-2025-02-18.sql
+++ db/patches/2.3.1-2025-02-18.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS areas(
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(255) NOT NULL,
   description VARCHAR(255),
   is_custom BOOL NOT NULL DEFAULT false,
