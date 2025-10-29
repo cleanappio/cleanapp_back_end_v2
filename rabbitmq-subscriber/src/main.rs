f03bf81e89fb52bf7dd5026abfe34c117416ccbc
Evgeny Koshelev <eko2000@gmail.com>
2025-10-29 13:04:03 +0100

    Remove async processing from the subscriber library

--- rabbitmq-subscriber/src/main.rs
+++ rabbitmq-subscriber/src/main.rs
@@ -1,5 +1,5 @@
 use chrono::{DateTime, Utc};
-use cleanapp_rustlib::rabbitmq::subscriber::{CallbackFunc, Message, Subscriber};
+use cleanapp_rustlib::rabbitmq::subscriber::{Callback, Message, Subscriber};
 use serde::{Deserialize, Serialize};
 use std::{collections::HashMap, sync::Arc};
 
@@ -22,21 +22,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut subscriber = Subscriber::new(amqp_url, "example_exchange", "example_queue").await?;
 
     // Define callback functions for different routing keys
-    let mut callbacks: HashMap<String, CallbackFunc> = HashMap::new();
+    let mut callbacks: HashMap<String, Arc<dyn Callback>> = HashMap::new();
     
     callbacks.insert(
         "example.routing.key".to_string(),
-        Arc::new(handle_example_message) as CallbackFunc,
+        Arc::new(ExampleCallback) as Arc<dyn Callback>,
     );
     
     callbacks.insert(
         "custom.routing.key".to_string(),
-        Arc::new(handle_custom_message) as CallbackFunc,
+        Arc::new(CustomCallback) as Arc<dyn Callback>,
     );
     
     callbacks.insert(
         "error.routing.key".to_string(),
-        Arc::new(handle_error_message) as CallbackFunc,
+        Arc::new(ErrorCallback) as Arc<dyn Callback>,
     );
 
     // Start consuming messages
@@ -61,37 +61,48 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 // handle_example_message processes messages with "example.routing.key" routing key
-fn handle_example_message(msg: &Message) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let example_msg: ExampleMessage = msg.unmarshal_to()?;
 
-    println!(
-        "Received example message: ID={}, Message={}, Time={}",
-        example_msg.id,
-        example_msg.message,
-        example_msg.timestamp.format("%Y-%m-%dT%H:%M:%S%.3fZ")
-    );
+struct ExampleCallback;
 
-    Ok(())
+impl Callback for ExampleCallback {
+    fn on_message(&self, msg: &Message) -> Result<(), Box<dyn std::error::Error>> {
+        let example_msg: ExampleMessage = msg.unmarshal_to()?;
+
+        println!(
+            "Received example message: ID={}, Message={}, Time={}",
+            example_msg.id,
+            example_msg.message,
+            example_msg.timestamp.format("%Y-%m-%dT%H:%M:%S%.3fZ")
+        );
+
+        Ok(())
+    }
 }
 
-// handle_custom_message processes messages with "custom.routing.key" routing key
-fn handle_custom_message(msg: &Message) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let example_msg: ExampleMessage = msg.unmarshal_to()?;
+struct CustomCallback;
 
-    println!(
-        "Received custom message: ID={}, Message={}, Time={}",
-        example_msg.id,
-        example_msg.message,
-        example_msg.timestamp.format("%Y-%m-%dT%H:%M:%S%.3fZ")
-    );
+impl Callback for CustomCallback {
+    fn on_message(&self, msg: &Message) -> Result<(), Box<dyn std::error::Error>> {
+        let custom_msg: ExampleMessage = msg.unmarshal_to()?;
 
-    Ok(())
+        println!(
+            "Received custom message: ID={}, Message={}, Time={}",
+            custom_msg.id,
+            custom_msg.message,
+            custom_msg.timestamp.format("%Y-%m-%dT%H:%M:%S%.3fZ")
+        );
+
+        Ok(())
+    }
 }
 
-// handle_error_message simulates an error scenario
-fn handle_error_message(msg: &Message) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    println!("Received error message: {}", String::from_utf8_lossy(&msg.body));
+struct ErrorCallback;
 
-    // Simulate an error - this will cause the message to be rejected
-    Err("simulated processing error".into())
+impl Callback for ErrorCallback {
+    fn on_message(&self, msg: &Message) -> Result<(), Box<dyn std::error::Error>> {
+        println!("Received error message: {}", String::from_utf8_lossy(&msg.body));
+
+        // Simulate an error - this will cause the message to be rejected
+        Err("simulated processing error".into())
+    }
 }
